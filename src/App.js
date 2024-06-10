9a2d0624c003299a4c5219a590c8435a5d275617
nick perez <nickperez1285@gmail.com>
2024-06-10 09:09:40 -0700

    added pgp

--- src/App.js
+++ src/App.js
@@ -38,6 +38,28 @@ class App extends Component {
        <h3>This is a simple message board that only saves 
         posts and nothing else . Its completely <u>annonymous</u> </h3>
        </center>
+       <center>
+        Nicks Public Key to receive messages  
+       <p style={{width: '50%' , border:'solid'}}>
+       <p>-----BEGIN PGP PUBLIC KEY BLOCK----- </p>
+<div>
+xjMEZmcdHBYJKwYBBAHaRw8BAQdAX8LX+HmXGWc5xJDU8j9hUS6ZYN09Gx67
+HcPa6JyxZ5vNJ3Zsbm14aGw4ampkbXZwZ3Fhd2ZnaHEgPG5pY2tAbmlnaHQu
+Y29tPsKMBBAWCgAdBQJmZx0cBAsJBwgDFQgKBBYAAgECGQECGwMCHgEAIQkQ
+mJV4WOqUS68WIQSrSIofDg65JVzXGIOYlXhY6pRLrxkCAP4mJJ9yPSAzPAhY
+HyY5fFurjpLuq/pie1ywKw2h+8GbmAEAxRtu4ilfJBQhUTjtclSxKcDGIVCi
+oNuwFMJtttdUhgTOOARmZx0cEgorBgEEAZdVAQUBAQdApNhqmfR/TnJep7ZT
+guISf1L/2Qji0oYGzEO9OLdoDFMDAQgHwngEGBYIAAkFAmZnHRwCGwwAIQkQ
+mJV4WOqUS68WIQSrSIofDg65JVzXGIOYlXhY6pRLr9jMAQCYyEf8oT2VA8NB
+80a1riEynjqN+RuLFYi+DgcBZzU3CAEA6c4ZKXbAUnNggpc9I/D9uqAHdU3t
+F92Pa14LbLfJUwQ==bT2/
+</div>
+<div>
+-----END PGP PUBLIC KEY BLOCK-----
+</div>
+
+</p>
+</center>
         <ThreadDisplay database={this.database} />
       </div>
     );
